f9ca61aa78e0f8db38acc7fbfa15ab3119a742ac
alugo002 <90716267+alugo002@users.noreply.github.com>
2022-04-26 14:46:48 -0400

    initial commit

--- zips.py
+++ zips.py
@@ -1,6 +1,5 @@
 import pymysql
-from flask import Flask, render_template, redirect, url_for
-from flask_bootstrap import Bootstrap
+from flask import Flask, render_template, redirect, url_for, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -10,22 +9,13 @@ from sqlalchemy.sql import text
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'checkMyZip'
-application = app
 
-username = 'alexehtb_zipCodeChecker'
-password = 'JesusWalks666'
-userpass = 'mysql+pymysql://' + username + ':' + password + '@'
-server  = '127.0.0.1'
-import os
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
-uri = os.getenv("DATABASE_URL")
-uri = uri.replace("postgres://", "postgresql://", 1)
-app.config['SQLALCHEMY_DATABASE_URI'] = uri
+db_name = 'allZips.db'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-Bootstrap(app)
 db = SQLAlchemy(app)
 
 zipsList = convert_to_dict("masterFile.csv")
@@ -40,26 +30,15 @@ for z in zipsList:
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    def testdb():
-        try:
-            db.session.query('1').from_statement(text('SELECT 1')).all()
-            return '<h1>It works.</h1>'
-        except Exception as e:
-            error_text = "<p>The error:<br>" + str(e) + "</p>"
-            hed = '<h1>Something is broken.</h1>'
-            return hed + error_text
     zips = get_zips(zipsList)
-    form = ZipForm()
-    message = ""
-    if form.validate_on_submit():
-        name = form.name.data
-        if name in zips:
-            form.name.data = ""
-            id = get_id(zipsList, name)
-            return redirect( url_for('detail', id=id) )
-        else:
-            message = "That zip code is not in our database."
-    return render_template('index.html', zips=zips, pairs=pairs_list, form=form, message=message)
+    code = request.form.get("zipCode")
+    if code in zips:
+        id = get_id(zips, code)
+        return redirect(url_for('zip', id=id))
+    else:
+        message = "That zip code is not in our database."
+
+    return render_template('index.html', zips=zips, pairs=pairs_list, message=message, code=code)
 
 @app.route('/zip/<num>')
 def detail(num):
